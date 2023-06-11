5486035d05e451e92307eadf5242809ab0562299
kaiquedsm <kaiquedsm15@gmail.com>
2023-06-11 15:11:38 -0300

    feat: Inserção de tabelas para listar as tarefas

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-let div_tasks = document.getElementById("tasks");
+let table_tasks = document.getElementById("table_tasks");
+let trs = document.getElementsByTagName('tr');
 let insert = document.getElementById("Inserir");
 let task_array = [];
 
 insert.addEventListener('click', inserir);
 
 function inserir() {
-    div_tasks.innerHTML = "";
+    table_tasks.innerHTML = 
+    `
+        <tr>
+            <th>Tarefa</th>
+            <th>Data</th>
+        </tr>
+    `;
     let task = document.getElementById("tarefa").value;
     let date = document.getElementById("data").value;
     let tarefa = { Nome: task, Data: date };
     task_array.push(tarefa);
     for (let i = 0; i < task_array.length; i++) {
-        div_tasks.innerHTML +=
+        table_tasks.innerHTML +=
         `
-            Tarefa: ${task_array[i].Nome}
-            Data: ${task_array[i].Data}
-            <button id="consultar">Consultar</button>
-            <button id="remover">Remover</button>
-            <br>
+            <tr>
+                <td>${task_array[i].Nome}</td>
+                <td>${task_array[i].Data}</td>
+                <td><button id="consultar">Consultar</button></td>
+                <td><button id="remover">Remover</button></td>
+                <br>
+            </tr>
         `
     }
 }
